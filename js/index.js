982c47189ad7ffbfb649d85f2546ffde8bbdd72d
unknown <paweladamian@gmail.com>
2021-02-14 22:55:35 +0100

    Adding lightbox to gallery

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const scroll = () => {
-  document.querySelector(".about-nav-link").addEventListener("click", window.scrollTo({
+  document.querySelector(".about-nav-link").addEventListener("click", document.body.scrollTo({
     top: 1500,
     left: 0,
     behavior: "smooth"
   })
-}
+  )}
