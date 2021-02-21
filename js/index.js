5f163f65a2f4544f00abb0d35812f76a05fb522c
unknown <paweladamian@gmail.com>
2021-02-21 20:41:08 +0100

    Adding fade-in effect with page scrolling

--- js/index.js
+++ js/index.js
@@ -1,7 +1,23 @@
-const scroll = () => {
-  document.querySelector(".about-nav-link").addEventListener("click", document.body.scrollTo({
-    top: 1500,
-    left: 0,
-    behavior: "smooth"
-  })
-  )}
+const checkpointFirst = document.querySelector(".horizontal-line-first");
+const checkpointSecond = document.querySelector(".horizontal-line-second");
+const checkpointThird = document.querySelector(".horizontal-line-third");
+
+const aboutHeader = document.querySelector(".section-about .heading-box");
+const aboutText = document.querySelector(".section-about .text-box");
+const offerHeader = document.querySelector(".section-offer .heading-box");
+const offerText = document.querySelector(".section-offer .text-box");
+
+window.addEventListener("scroll", function() {
+  if (window.pageYOffset >  checkpointFirst.offsetTop) {
+    aboutHeader.classList.add("show");
+    aboutText.classList.add("show");
+  }
+})
+
+window.addEventListener("scroll", function() {
+  if (window.pageYOffset >  checkpointSecond.offsetTop) {
+    offerHeader.classList.add("show");
+    offerText.classList.add("show");
+  }
+})
+
